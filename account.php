ab697668715bc7e8871e365ae14369ebe58290fa
jb695 <77747647+jb695@users.noreply.github.com>
2021-11-15 19:14:15 -0500

    Update account.php

--- account.php
+++ account.php
@@ -4,9 +4,9 @@
 //Open in Notepad++
 //CHANGE THE FILE EXTENSION TO .php
 
-$hostname = "sql.njit.edu"     ; 	  // or "sql2.njit.edu"   OR "sql1.njit.edu"
-$username = "jb695" ;
-$project  = "jb695" ;
-$password = "Zoidberg1$" ;
+$hostname = "sql.***.edu"     ; 	  
+$username = "jb***" ;
+$project  = "jb***" ;
+$password = "Z*********" ;
 
 ?>
